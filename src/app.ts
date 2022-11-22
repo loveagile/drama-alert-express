95c3c0f4e929f55f95429fb836ca4049e4c583b0
landry <happyplandry@gmail.com>
2022-11-22 05:10:11 -0500

    fixed: database

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import * as express from 'express'
-import { createConnection } from 'typeorm'
-import { Request, Response } from 'express'
 import * as bodyParser from 'body-parser'
 
-import { Routes } from './routes'
+import { AppDataSource } from './utils/data-source'
+import auth from './routes/auth'
 
-createConnection()
-  .then(async (connection) => {
+AppDataSource.initialize()
+  .then(async () => {
     const app = express()
     const port = process.env.PORT || 4002
     app.use(express.static('public'))
@@ -14,30 +13,9 @@ createConnection()
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(bodyParser.json())
 
-    Routes.forEach((route) => {
-      ;(app as any)[route.method](
-        route.route,
-        (req: Request, res: Response, next: Function) => {
-          const result = new (route.controller as any)()[route.action](
-            req,
-            res,
-            next
-          )
-          if (result instanceof Promise) {
-            result.then((result) =>
-              result !== null && result !== undefined
-                ? res.send(result)
-                : undefined
-            )
-          } else if (result !== null && result !== undefined) {
-            res.json(result)
-          }
-        }
-      )
-    })
-
+    app.use('/api/auth', auth)
     app.listen(port, () =>
       console.log(`REST API server ready at: http://localhost:${port}`)
     )
   })
-  .catch((error) => console.log('TypeORM connection error: ', error))
+  .catch((error) => console.log(error))
