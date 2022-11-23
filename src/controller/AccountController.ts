8e91b40d4dc12a79022b9b8926ac78430b67bcfb
landry <happyplandry@gmail.com>
2022-11-23 07:48:58 -0500

    added: feature of creating an account

--- src/controller/AccountController.ts
+++ src/controller/AccountController.ts
@@ -1,13 +1,34 @@
 import { Request, Response } from 'express'
+import { JwtPayload } from 'jsonwebtoken'
 
-import { getAllAccounts } from '../service/accounts'
+import { User } from '../utils/types'
+import { getAllAccounts, createAccount } from '../service/accounts'
 import { getErrorMessage } from '../utils/errors'
+
+export interface CustomRequest extends Request {
+  token: string | JwtPayload
+  user: User
+}
 
 export const getAccounts = async (req: Request, res: Response) => {
   try {
     const accounts = await getAllAccounts()
-    res.status(200).send(accounts)
+    return res.status(200).send(accounts)
   } catch (error) {
-    res.status(500).send(getErrorMessage(error))
+    return res.status(500).send(getErrorMessage(error))
+  }
+}
+
+export const addAccount = async (req: CustomRequest, res: Response) => {
+  try {
+    if (req.user.role !== 'admin') {
+      return res.status(403).send({ message: 'No authentication' })
+    }
+    const urlname = req.body.fullname.split(' ').join('').toLowerCase()
+    const account = { ...req.body, urlname }
+    const newAccount = await createAccount(account)
+    return res.status(200).send({ account: newAccount })
+  } catch (error) {
+    return res.status(500).send(getErrorMessage(error))
   }
 }
--- src/routes/accounts.ts
+++ src/routes/accounts.ts
@@ -1,10 +1,11 @@
 import * as express from 'express'
 
-import { getAccounts } from '../controller/AccountController'
+import { getAccounts, addAccount } from '../controller/AccountController'
 import { auth } from '../middleware/auth'
 
 const router = express.Router()
 
 router.get('/', auth, getAccounts)
+router.post('/', auth, addAccount)
 
 export default router
