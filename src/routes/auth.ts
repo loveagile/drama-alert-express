1870c1ee881a09af5f0ed1f0749db92a0d33a88a
landry <happyplandry@gmail.com>
2022-12-19 10:56:11 -0500

    added: comment

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,12 @@
 import * as express from 'express'
 
-import { login, register } from '../controller/AuthController'
+import { login, register, getCurrentUser } from '../controller/AuthController'
+import { auth } from '../middleware/auth'
 
 const router = express.Router()
 
 router.post('/login', login)
 router.post('/register', register)
+router.get('/currentuser', auth, getCurrentUser)
 
 export default router
